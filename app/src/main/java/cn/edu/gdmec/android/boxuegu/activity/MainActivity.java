5cc94b52583c91dee6bcbbd64042ff4fcd0d7223
YEHong1 <870628496@qq.com>
2018-03-20 22:25:46 +0800

    注册与登录功能的实现

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/MainActivity.java
@@ -1,7 +1,13 @@
 package cn.edu.gdmec.android.boxuegu.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import cn.edu.gdmec.android.boxuegu.R;
 
@@ -11,6 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Timer timer=new Timer();
+        //TimerTask类表示一个在指定时间内执行的task
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent intent=new Intent(MainActivity.this,LoginAcitivity.class);
+                startActivity(intent);
+
+                MainActivity.this.finish();
+            }
+        };
+        //设置这个task在延迟3秒之后自动执行
+        timer.schedule(task,3000);
     }
 
 }
