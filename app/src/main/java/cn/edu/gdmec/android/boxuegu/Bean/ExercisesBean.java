359e2b3921429ac446e7eb764daabf52db5b7604
YEHong1 <870628496@qq.com>
2018-04-20 17:54:56 +0800

    adapter未完待续

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/Bean/ExercisesBean.java
@@ -0,0 +1,20 @@
+package cn.edu.gdmec.android.boxuegu.Bean;
+
+/**
+ * Created by Curtain_Liang on 2018/4/20.
+ */
+
+public class ExercisesBean {
+    public int id;
+    public String title;
+    public String content;
+    public int background;
+    public int subjectId;
+    public String subject;
+    public String a;
+    public String b;
+    public String c;
+    public String d;
+    public int answer;
+    public int select;
+}
